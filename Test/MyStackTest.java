c2a02cb158c2c513f141fffd4d1804d34aebee6e
Rithvik26 <golthirithvik78@gmail.com>
2023-03-30 11:33:54 -0500

    Added first test case

--- Test/MyStackTest.java
+++ Test/MyStackTest.java
@@ -1,10 +1,16 @@
+import static org.junit.Assert.*;
 import org.junit.*;
 public class MyStackTest {
-
     private MyStack stack;
-
     @Before
     public void setUp() {
         stack = new MyStack();
+    }
+
+    // Equivalence partitioning tests
+    @Test
+    public void testEmptyStack() {
+        // Partition: empty stack
+        assertTrue(stack.empty());
     }
 }
