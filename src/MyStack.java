2c007b6962696ff2609639f42a5b411532b64f4c
Rithvik26 <golthirithvik78@gmail.com>
2023-03-30 11:35:52 -0500

    Developed Mystack code to test the first test case written

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,3 +1,12 @@
 public class MyStack {
+    private Object[] stack;
+    private int top;
+    public MyStack() {
+        stack = new Object[100];
+        top = -1;
+    }
 
+    public boolean empty() {
+        return top == -1;
+    }
 }
